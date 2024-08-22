ab8afe82da76c17f770755514b2c6bd9bd010ba4
k355l3r-5yndr0m3 <hoanghung17jan@gmail.com>
2024-08-22 15:28:03 +0700

    Basic login page

--- frontend/src/app/modules/login/login.component.ts
+++ frontend/src/app/modules/login/login.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  username: string = "username"
+  password: string = "password"
 
+  onSubmit(): void {
+    alert(`username: ${this.username}\npassword: ${this.password}`)
+  }
 }
