1475293f79fb39d7d55da30a75e32dfb54dbbb17
zzcongkunzz <dangcong200502@gmail.com>
2024-08-26 17:33:38 +0700

    Configure database connection (Error:"No DbContext named 'Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityDbContext`3' was found.")

--- api/QLThuVien.Data/Data/AppDbContext.cs
+++ api/QLThuVien.Data/Data/AppDbContext.cs
@@ -7,10 +7,20 @@ namespace QLThuVien.Data.Data;
 
 public class AppDbContext : IdentityDbContext<User, Role, Guid>
 {
+    #region DbSet
+    
     public DbSet<User> Users { get; set; }
     public DbSet<UserRole> UserRoles { get; set; }
     public DbSet<Role> Roles { get; set; }
+    
+    #endregion
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
 
+    }
+
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
