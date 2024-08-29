49dbd8e90657599244a012728b402570646a3d10
hienvm <hien2842003@gmail.com>
2024-08-29 10:02:22 +0700

    update layout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,16 +3,15 @@ import {BaseLayoutComponent} from './modules/shared/layouts/base-layout/base-lay
 
 export const routes: Routes = [
   {
+    // for no-users
     path: 'auth',
     loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule),
   },
   {
-    path: 'auth',
-    loadChildren: () => import('./modules/base/users/users.module').then(m => m.UsersModule),
-  },
-  {
+    // for logged-in user
     path: '',
     component: BaseLayoutComponent,
+    title: 'Thư viện số',
     loadChildren: () => import('./modules/base/base.module').then(m => m.BaseModule)
   },
 ];
